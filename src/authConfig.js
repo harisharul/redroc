5134528caa505513cfc3ca788afc370f3ad441c6
Harish Arulkumar <harisharul@gmail.com>
2026-01-12 05:22:55 +0000

    updated client id

--- src/authConfig.js
+++ src/authConfig.js
@@ -1,6 +1,6 @@
 export const msalConfig = {
   auth: {
-    clientId: "caedbd5c-412c-42e6-a596-05b73e1f5579",
+    clientId: "ce384644-f524-4f94-a38e-f8f92d589fad",
     authority: "https://redrocin.ciamlogin.com/caedbd5c-412c-42e6-a596-05b73e1f5579",
     knownAuthorities: ["redrocin.ciamlogin.com"],
     redirectUri: "/auth/callback"
